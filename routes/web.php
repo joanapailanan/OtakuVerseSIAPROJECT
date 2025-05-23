4b713c09cdef5ce4ae514825945caa1acadf64b7
PinedaJoshua <pineda.joshua0416@gmail.com>
2025-05-23 22:54:57 +0800

    fixed web route

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\RedditController;
-//use App\Http\Controllers\AnimeController;
-//use App\Http\Controllers\SearchAnimeController;
-// If needed, you can include this instead or combine logic
-//use App\Http\Controllers\AnimeSearchController;
 use App\Http\Controllers\MediaController;
-use App\Http\Controllers\AnimeQuoteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-// Reddit routes
-Route::get('/reddit', [RedditController::class, 'show']);
-Route::get('/reddit/{subreddit?}', [RedditController::class, 'show']);
-
-// Anime search route (choose one controller)
-Route::get('/anime/search', [SearchAnimeController::class, 'search'])->name('anime.search');
-// Alternatively, if you prefer this one:
-Route::get('/search/anime', [AnimeSearchController::class, 'search'])->name('anime.search.advanced');
-*/
-
-Route::get('/search/anime', [MediaController::class, 'searchWithJikan'])->name('anime.search.advanced');
+// Anime search routes
 Route::get('/anime/search', [MediaController::class, 'searchWithAnilistKitsu'])->name('anime.search');
-Route::get('/reddit/{subreddit?}', [MediaController::class, 'showReddit'])->name('reddit.show');
-Route::get('/reddit', [MediaController::class, 'showReddit'])->name('reddit.show');
+Route::get('/search/anime', [MediaController::class, 'searchWithJikan'])->name('anime.search.advanced');
 
-// Anime Quotes routes
+// Reddit routes
+Route::get('/reddit', [MediaController::class, 'showReddit'])->name('reddit.show');
+Route::get('/reddit/{subreddit?}', [MediaController::class, 'showReddit'])->name('reddit.show');
+
+// Anime quote route
 Route::get('/anime-quote', [MediaController::class, 'getRandomQuote'])->name('anime.quote');
