7cad637838e2ae27783d255fd17e6cbfd1c36c4d
PinedaJoshua <pineda.joshua0416@gmail.com>
2025-05-23 19:41:47 +0800

    merged reddit api and animesearch route

--- app/Http/Controllers/RedditController.php
+++ app/Http/Controllers/RedditController.php
@@ -6,12 +6,22 @@ use App\Services\RedditService;
 
 class RedditController extends Controller
 {
-    public function show(RedditService $reddit)
+    public function show(RedditService $reddit, $subreddit = null)
 {
-    $subreddits = ['animemes', 'goodanimemes', 'animememes', 'wholesomeanimemes'];
+    if ($subreddit) {
+        // If a specific subreddit is passed
+        $subreddits = [$subreddit];
+    } else {
+        // Default to multiple subreddits
+        $subreddits = ['animemes', 'goodanimemes', 'animememes', 'wholesomeanimemes'];
+    }
 
     $posts = $reddit->getSubredditPosts($subreddits);
 
-    return view('reddit', ['posts' => $posts['data']['children']]);
+    return view('reddit', [ 
+        'posts' => $posts['data']['children'],
+        'subreddits' => $subreddits
+    ]);
 }
+
 }
