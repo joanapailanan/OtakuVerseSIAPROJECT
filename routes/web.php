d854bb2485d8cae8d7fdcba34faf6cb94847e865
PinedaJoshua <pineda.joshua0416@gmail.com>
2025-05-23 23:01:15 +0800

    Add files via upload
    updated web

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MediaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Anime search routes
+Route::get('/anime/search', [MediaController::class, 'searchWithAnilistKitsu'])->name('anime.search');
+Route::get('/search/anime', [MediaController::class, 'searchWithJikan'])->name('anime.search.advanced');
+
+// Reddit routes
+Route::get('/reddit', [MediaController::class, 'showReddit'])->name('reddit.show');
+Route::get('/reddit/{subreddit?}', [MediaController::class, 'showReddit'])->name('reddit.show');
+
+// Anime quote route
+Route::get('/anime-quote', [MediaController::class, 'getRandomQuote'])->name('anime.quote');
